13f9104685808507dde605d71fa21d2197795df6
BenDehlin <ben.dehlin@gmail.com>
2020-05-20 00:12:48 -0600

    add Header functionality for logged in vs logged out user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react"
 import "./App.css"
-import routes from './routes'
-import Header from './Components/Header'
+import routes from "./routes"
+import Header from "./Components/Header"
+import {useLocation} from 'react-router-dom'
 
-const App = () => (
-  <div className="App">
-    <Header />
-    {routes}
-  </div>)
+const App = () => {
+  const {pathname} = useLocation()
+  return (
+    <div className="App">
+      {pathname !== '/login' && pathname !== '/register' && <Header />}
+      {routes}
+    </div>
+  )
+}
 
 export default App
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,25 @@
-import React, {useState} from 'react'
+import React, { useState } from "react"
+import { useHistory } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 const Header = () => {
+  const { push } = useHistory()
+  const { user } = useSelector(({ authReducer }) => authReducer)
   return (
-    <div>Header</div>
+    <header>
+      {user && user.user_id && (
+        <div>
+          <button onClick={() => push("/dashboard")}>Dashboard</button>
+          <button onClick={() => push("/userlist")}>User List</button>
+        </div>
+      )}
+      {!user || !user.user_id && (
+          <div>
+            <button onClick={() => push("/login")}>Login</button>
+            <button onClick={() => push("/register")}>Register</button>
+          </div>
+        )}
+    </header>
   )
 }
 
